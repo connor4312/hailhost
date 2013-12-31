549aad1ec6360024a94596e071c0022a789767ec
Connor Peet <connor@connorpeet.com>
2013-12-31 09:43:21 -0500

    Images, slimrr updates

--- dist/index.php
+++ dist/index.php
@@ -5,8 +5,7 @@ session_start();
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim;
-$app->config('defaultLanguage', 'en');
-$app->config('baseRoute', '/g/CraftYourBox-Site/dist');
+$app->config('baseRoute', '/g/hailhost/dist');
 
 // Register a singleton of the Mustache engine, and tell it to cache
 $app->container->singleton('twig', function () {
@@ -34,43 +33,43 @@ function renderTemplate($name, $data = array()) {
 }
 // Loads a page by the given name/path
 function loadPage($path) {
-        global $app;
+	global $app;
 
-        // Set up the base path
-        $f = 'pages/' . $path;
-        
-        if (file_exists($f . '.html')) {
-                // If there's an HTML file, render the mustache template
-                return renderTemplate($path . '.html');
-        } elseif (file_exists($f . '.php')) {
-                // If there's a PHP file, return it
-                return (require $f . '.php');
-        } elseif ($path != '404') {
-                // Otherwise, go get the 404 page
-                return loadPage('404');
-        } else {
-                // But if the user doesn't have a 404 page made, return a plain 404
-                $app->halt(404);
-        }
+	// Set up the base path
+	$f = 'pages/' . $path;
+	
+	if (file_exists($f . '.html')) {
+		// If there's an HTML file, render the mustache template
+		return renderTemplate($path . '.html');
+	} elseif (file_exists($f . '.php')) {
+		// If there's a PHP file, return it
+		return (require $f . '.php');
+	} elseif ($path != '404') {
+		// Otherwise, go get the 404 page
+		return loadPage('404');
+	} else {
+		// But if the user doesn't have a 404 page made, return a plain 404
+		$app->halt(404);
+	}
 }
 
 // Index page
 $app->get('/', function () use ($app) {
-        echo loadPage('index');
+	echo loadPage('index');
 });
 
 
 // Route to everything else
 $app->get('/.*?', function () use ($app) {
-        // Get the current request path
-        $path = $app->request->getResourceUri();
+	// Get the current request path
+	$path = $app->request->getResourceUri();
 
-        // Make sure the user isn't trying to escape and do nasty things
-        if (!preg_match('/^[A-z\/\-\_]+$/', $path)) {
-                echo loadPage('404');
-        }
+	// Make sure the user isn't trying to escape and do nasty things
+	if (!preg_match('/^[A-z\/\-\_]+$/', $path)) {
+		echo loadPage('404');
+	}
 
-        // Send the page off to get loaded
-        echo loadPage($path);
+	// Send the page off to get loaded
+	echo loadPage($path);
 });
 $app->run();
